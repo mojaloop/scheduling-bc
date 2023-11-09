04d7b66c13f1f209c2dc7fd2a14b8299acca656e
Pedro Sousa Barreto <pedrosousabarreto@gmail.com>
2023-11-09 22:18:54 +0000

    Changed default port from 1234 to 3150

--- packages/scheduling-svc/src/application/service.ts
+++ packages/scheduling-svc/src/application/service.ts
@@ -99,11 +99,11 @@ const DB_NAME = process.env.SCHEDULING_DB_NAME ?? "scheduling";
 const MONGO_URL = process.env["MONGO_URL"] || "mongodb://root:mongoDbPas42@localhost:27017/";
 
 // Express Server
-const SVC_DEFAULT_HTTP_PORT = process.env["SVC_DEFAULT_HTTP_PORT"] || 1234;
+const SVC_DEFAULT_HTTP_PORT = process.env["SVC_DEFAULT_HTTP_PORT"] || 3150;
 
 // Auth Requester
 // const SVC_CLIENT_ID = process.env["SVC_CLIENT_ID"] || "scheduling-bc-scheduling-svc";
-// const SVC_CLIENT_SECRET = process.env["SVC_CLIENT_ID"] || "superServiceSecret";
+// const SVC_CLIENT_SECRET = process.env["SVC_CLIENT_SECRET"] || "superServiceSecret";
 
 // const AUTH_N_SVC_BASEURL = process.env["AUTH_N_SVC_BASEURL"] || "http://localhost:3201";
 // const AUTH_N_SVC_TOKEN_URL = AUTH_N_SVC_BASEURL + "/token"; // TODO this should not be known here, libs that use the base should add the suffix
